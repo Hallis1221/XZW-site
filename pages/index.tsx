cb7648c4f2a724d10dd8fe51a9866188d5ebd19e
Halvor Vivelstad <halvorviv@gmail.com>
2022-06-07 17:12:44 +0200

    Configure frontend

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+  <div className='text-3xl font-bold underline'>
+    Hello. 
+  </div>
+  )
+}
+
+export default Home
