d67619188ffbba23c7bc2e085d8f9351fd35be0d
Halvor Vivelstad <halvorviv@gmail.com>
2022-06-11 17:48:31 +0200

    :accept: Accesibility

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html
+      lang="no-NB"
+    >
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
