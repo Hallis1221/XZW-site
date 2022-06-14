82700b67e8484d937ed2ecd8df46a2a378891e45
Halvor Vivelstad <halvorviv@gmail.com>
2022-06-14 12:26:42 +0200

    preventScrollOnSwipe: true,

--- components/swipeAbleFlashcard/component.tsx
+++ components/swipeAbleFlashcard/component.tsx
@@ -4,36 +4,48 @@ import { Glose } from "types/glose";
 import { Flashcard } from "components/flashcard";
 import { SwipeAbleFlashcardProps } from "./props";
 
-export function SwipeableFlashcard({flipped, setFlipped, glose, onLeft, onRight}: SwipeAbleFlashcardProps) {
-    const [swipeData, setSwipeData] = useState<SwipeEventData | null>(null);
-  
-    const handlers = useSwipeable({
-      onSwipedLeft: () => onLeft(),
-      onSwipedRight: () => onRight(),
-      onSwiping: (e) => {
-        if (e == swipeData) console.log("swiping");
-        setSwipeData(e);
-      },
-      onTouchEndOrOnMouseUp: () => {
-        setSwipeData(null);
-      },
-    });
-    return <div
-    {...handlers}
+export function SwipeableFlashcard({
+  flipped,
+  setFlipped,
+  glose,
+  onLeft,
+  onRight,
+}: SwipeAbleFlashcardProps) {
+  const [swipeData, setSwipeData] = useState<SwipeEventData | null>(null);
+
+  const handlers = useSwipeable({
+    preventScrollOnSwipe: true,
+    onSwipedLeft: () => onLeft(),
+    onSwipedRight: () => onRight(),
+    onSwiping: (e) => {
+      if (e == swipeData) console.log("swiping");
+      setSwipeData(e);
+    },
+    onTouchEndOrOnMouseUp: () => {
+      setSwipeData(null);
+    },
+  });
+  return (
+    <div
+      {...handlers}
       className="w-full h-full relative"
-      style={swipeData
-        ? {
-          zIndex: 999999,
-          position: "absolute",
-          transform: `translateY(${swipeData?.deltaY}px) translateX(${swipeData?.deltaX}px)`,
-          rotate: `${swipeData?.deltaX/15}deg`,
-        }
-        : {}}
+      style={
+        swipeData
+          ? {
+              zIndex: 999999,
+              position: "absolute",
+              transform: `translateY(${swipeData?.deltaY}px) translateX(${swipeData?.deltaX}px)`,
+              rotate: `${swipeData?.deltaX / 15}deg`,
+            }
+          : {}
+      }
     >
       <Flashcard
         flipped={flipped}
         onClick={() => setFlipped(!flipped)}
         front={glose.Standard}
-        back={glose.Chinese} />
-    </div>;
-  }
+        back={glose.Chinese}
+      />
+    </div>
+  );
+}
